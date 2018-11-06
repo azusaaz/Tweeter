aecfbdf77073dafc918da9f4b967031d3b5c07f0
“az” <shimazaki@aaa.email.ne.jp>
2018-11-06 17:42:05 -0500

    add text counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+  $(".new-tweet form textarea").on('keyup', function () {
+    var $textarea = $(this);
+    var count = 140 - $textarea.val().length;
+    var $counter = $textarea.siblings('.counter')
+
+    if (count < 0) {
+      $counter.addClass('error');
+    } else {
+      $counter.removeClass('error');
+    }
+    $counter.html(count);
+  });
+});
+
+// before refactor
+// $(document).ready(function () {
+
+//   $(".new-tweet form textarea").on('keydown', function () {
+//     var $textarea = $(this);
+//     var max = 140;
+//     var txt = $textarea.val().length + 1;
+//     var $counter = $textarea.siblings('.counter')
+
+//     if (max - txt < 0) {
+//       $textarea.siblings('.counter').css('color', 'red');
+//     }
+//     $counter.html(max - txt);
+//   });
+// });
