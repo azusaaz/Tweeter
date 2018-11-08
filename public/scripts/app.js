f79426793c90c1bd484a740c9e474b458080fb40
“az” <shimazaki@aaa.email.ne.jp>
2018-11-08 14:20:41 -0500

    beutify file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,55 @@
  */
 $(document).ready(function () {
 
+  //slide toggle the new tweet window
+  $("#nav-button").on('click', function () {
+    if ($("#new-tweet").is(':visible')) {
+      $("#new-tweet").slideUp();
+
+    } else {
+      $("#new-tweet").slideDown();
+      $("#new-tweet textarea").focus();
+
+    }
+  });
+
+  //when clicked tweet button
+  $("form").on('submit', function (event) {
+    event.preventDefault();
+    // console.log("serial",$( this ).serialize());
+
+    if ($(this).find("textarea").val().length === 0) {
+      $('#error').html("Please input something");
+      $('#error').slideDown();
+
+    } else if ($(this).find("textarea").val().length > 140) {
+      $('#error').html("Cannot tweet more than 140 letters");
+      $('#error').slideDown();
+
+    } else {
+
+      var $text = $(this).find("textarea");
+
+      $.ajax({
+          url: 'http://localhost:8080/tweets',
+          method: 'POST',
+          data: $(this).serialize(),
+        })
+
+        .done(function (data, status, response) {
+          // console.log(data,status,response.status)
+          if (response.status === 503) {
+            alert("something wrong");
+          }
+          if (response.status === 201) {
+            $('#tweets-container').empty();
+            $text.val("");
+            loadTweets();
+          }
+        });
+    }
+  });
+
   function createTweetElement(data) {
     var daysAgo = Math.floor((Date.now() - data.created_at) / 1000 / 86400);
 
@@ -46,55 +95,6 @@ $(document).ready(function () {
       $('#tweets-container').append($tweet);
     }
   }
-
-  //slide toggle the new tweet window
-  $("#nav-button").on('click', function () {
-    if ($("#new-tweet").is(':visible')) {
-      $("#new-tweet").slideUp();
-    } else {
-      $("#new-tweet").slideDown();
-      $("#new-tweet textarea").focus();
-    }
-  });
-
-  $("form").on('submit', function (event) {
-    event.preventDefault();
-    // console.log("serial",$( this ).serialize());
-    
-    if ($(this).find("textarea").val().length === 0) {
-      $('#error').html("Please input something");
-      $('#error').slideDown();
- 
-    }else if ($(this).find("textarea").val().length > 140) {
-      $('#error').html("Cannot tweet more than 140 letters");
-      $('#error').slideDown();
-   
-    }else{
-  
-
-    var $text = $(this).find("textarea");
-
-
-    $.ajax({
-        url: 'http://localhost:8080/tweets',
-        method: 'POST',
-        data: $(this).serialize(),
-      })
-
-      .done(function (data, status, response) {
-        // console.log(data,status,response.status)
-        if (response.status === 503) {
-          alert("something wrong");
-        }
-        if (response.status === 201) {
-          $('#tweets-container').empty();
-          $text.val("");
-          loadTweets();
-        }
-
-      });
-    }
-  });
 
   function loadTweets() {
 
